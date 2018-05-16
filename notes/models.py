2cc8916ea5d504250ea3659502dfc365ec3191a1
tesest <v.efimenko@prom.ua>
2018-05-16 17:46:15 +0300

    first

---
+++ notes/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from django.utils import timezone
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
